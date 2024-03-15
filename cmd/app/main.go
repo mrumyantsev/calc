774cb4de0f1e78b77be5926746786ab739f253ff
Mikhail Rumyantsev <mi.rumyantsev.2020@gmail.com>
2024-03-15 20:37:25 +0300

    refactor: rename application package and remake

--- cmd/app/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import ec "app/internal/exprcalc"
-
-func main() {
-	calculatorCircuitBoard := ec.NewCalculatorCircuitBoard()
-
-	ec.PowerOn(calculatorCircuitBoard)
-}
---
+++ cmd/calc/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/mrumyantsev/calc/internal/exprcalc"
+
+func main() {
+	calc := exprcalc.New()
+
+	calc.Start()
+}
